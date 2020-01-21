01895e7f874078dec48691ff208bd4303c7718f4
DESKTOP-PC <briefkst@gmail.com>
2020-01-20 20:13:43 -0500

    controller repository Grud Operation simple

---
+++ README.md
@@ -0,0 +1,6 @@
+:spring_boot_version: 2.2.2.RELEASE
+:icons: font
+:source-highlighter: prettify
+:project_id: jpa mysql connection
+Phone Book
+
---
+++ src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.accessingdatamysql.User;
+
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
